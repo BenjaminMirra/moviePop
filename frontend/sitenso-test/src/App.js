bcdd853dcb6eda47d9ccc53cfa94f5015bf21c30
Benjamin Mirra <mirrabenjamin@gmail.com>
2022-11-29 00:20:34 -0300

    testing api

--- frontend/sitenso-test/src/App.js
+++ frontend/sitenso-test/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import { Login } from './components/Organisms/Login';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hola mundo</h1>
+      <Login/>
     </div>
   );
 }
